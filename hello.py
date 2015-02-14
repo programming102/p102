264b68c8b1597af93233d68e297fef037e8f9ece
@ProfNandaa <profnandaa@gmail.com>
2015-02-14 11:54:25 +0300

    Added Sum function, close #1

--- hello.py
+++ hello.py
@@ -3,7 +3,9 @@ print "Hello World!"
 x = input("Value of A: ")
 y = input("Value of B: ")
 
-print "A + B = ",(x+y)
+print "A + B = ",my_sum(x,y)
 
 print "I've changed something"
 
+def function my_sum(a,b):
+	return a+b
